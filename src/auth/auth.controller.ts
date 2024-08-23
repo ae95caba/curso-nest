5dfd7cdfcdbc0be1c1d962fb514fc2c29c72e05a
ae95caba <ae95caba@gmail.com>
2024-08-23 03:08:54 -0300

    create auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller({})
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Get('/auth')
   getAuth() {
-    return 'this is auth';
+    return this.authService.getAuth();
   }
 }
